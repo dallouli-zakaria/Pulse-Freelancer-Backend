8d5aff1b11cb807ebd8412c4ed1556fdd573d699
zakaria <dalloulizakaria@gmail.com>
2024-05-06 11:57:23 +0100

    adding freelancer

---
+++ app/Models/FreelanceModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FreelanceModel extends Model
+{
+    use HasFactory;
+}
