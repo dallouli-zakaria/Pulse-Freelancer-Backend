53f720d15bc925ef17a2e8b645d8975973c5a558
zakaria <dalloulizakaria@gmail.com>
2024-05-16 16:22:49 +0100

    add role

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role;
 
 class Permission extends Model
 {
