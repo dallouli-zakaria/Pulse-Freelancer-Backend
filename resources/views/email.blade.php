8b4d78afd59e7f379e144f82310c541bc683c44d
haidasse <j.haidasse@upm.ac.ma>
2024-05-17 09:45:01 +0100

    mailing

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,6 +1,4 @@
-<h1>hello ras taro </h1>
-<p>baghi  tkhdm m3ana
-    chorot dyalna 
-    makhskch tkon bnadem otkhdm 24/24 safi
+<h1>hello  </h1>
+<p>Please {{$name}} comfer {{$email}} is yur email
 </p>
-<h2>this your comfermation code {{ $random }} </h2>
+<h2>this your comfermation code {{$random}} </h2>
