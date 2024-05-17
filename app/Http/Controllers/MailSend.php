0889aa0abcae1d8ef021eb6ac7a9028ae0e12743
haidasse <j.haidasse@upm.ac.ma>
2024-05-17 10:39:54 +0100

    last changes

--- app/Http/Controllers/MailSend.php
+++ app/Http/Controllers/MailSend.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 
 
 
@@ -11,13 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-=======
-use App\Mail\ProfileMail;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
->>>>>>> ab68d83de276447d5a74118da53dff0a4844acb8
 
 class MailSend extends Controller
 {
@@ -28,11 +20,7 @@ class MailSend extends Controller
         try {
             
              $user = User::where("id",$request->id)->firstOrFail();
-<<<<<<< HEAD
             Mail::to($user->email)->send(new ProfilMail($user));
-=======
-            Mail::to($user->email)->send(new ProfileMail($user));
->>>>>>> ab68d83de276447d5a74118da53dff0a4844acb8
             return response()->json("message sedde to".$user->email, 200);
         } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'User not found'], 404);
