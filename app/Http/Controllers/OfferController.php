316f1957491ac8b884fa3fe104068177e31f5220
zakaria <dalloulizakaria@gmail.com>
2024-09-13 10:29:00 +0100

    fixing bugs 4

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -28,23 +28,47 @@ class OfferController extends Controller
         }
     }
 
-    public function store(Request $request)
-    {
-        try {
-            $request->validate([
-                'selected'=>'required|string',
-                'freelancer_id'=>'required|numeric',
-                'post_id'=>'required|numeric'
-            ]);
+    // public function store(Request $request)
+    // {
+    //     try {
+    //         // Validation des données d'entrée
+    //         $request->validate([
+    //             'selected' => 'required|string',
+    //             'freelancer_id' => 'required|numeric',
+    //             'post_id' => 'required|numeric'
+    //         ]);
+    
+    //         // Création de l'offre
+    //         $offer = Offer::create($request->all());
+    
+    //         // Récupération du freelancer, du post, et du client
+    //         $freelancer = User::findOrFail($request->freelancer_id);
+    //         $post = Post::findOrFail($request->post_id);
+    //         $client = User::where('id',$post->client_id)->first();
+    
+    //         // Préparation des données pour les notifications
+    //         $clientName = $client->name;
+    //         $postTitle = $post->title;
+    //         $userName = $freelancer->name;
+    
+    //         // Envoi des notifications
+    //         try {
+    //             $freelancer->notify(new CandidateSended($userName, $postTitle));
+    //             $client->notify(new NewCandidateApply($clientName, $postTitle));
+    //         } catch (\Exception $e) {
+    //             return response()->json(['error' => $e], 500);
+    //         }
+    
+    //         return response()->json($offer, 201);
+    //     } catch (ValidationException $e) {
+    //         return response()->json(['errors' => $e->errors()], 422);
+    //     } catch (ModelNotFoundException $e) {
+    //         return response()->json(['error' => 'Resource not found.'], 404);
+    //     } catch (\Exception $e) {
             
-            $offer = Offer::create($request->all());
-            return response()->json($offer, 201);
-        } catch (ValidationException $e) {
-            return response()->json(['errors' => $e->errors()], 422);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e], 500);
-        }
-    }
+    //         return response()->json(['error' => $e], 500);
+    //     }
+    // }
     
 
     
